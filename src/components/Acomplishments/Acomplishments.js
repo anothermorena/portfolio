ab19e9392dad004cf4a59ef6fce652606025748f
morenamt <onalepeloo@live.com>
2022-08-02 12:21:04 +0200

    completed the personal achievements section

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,19 +1,27 @@
 import React from 'react';
-
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: 7, text: 'Deans List Awards'},
+  { number: 3, text: 'Best Student Awards', },
+  { number: 1, text: 'Fully Funded Masters Degree Scholarship Award', },
+  { number: 10, text: 'Certificates of Execellence', }
 ];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+  <Section>
+    <SectionTitle>Personal Achievements</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{`${card.number}`}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+    <SectionDivider/>
+  </Section>
 );
 
 export default Acomplishments;
