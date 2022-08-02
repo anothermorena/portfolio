898d736775af2e24ffc3bf64188f089faa1d5cab
morenamt <onalepeloo@live.com>
2022-08-02 12:15:20 +0200

    completed the time line section

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -56,9 +56,9 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2016, text: 'Started my software engineering journey.', },
+  { year: 2016, text: 'Got an information technology internship at Always Open Clinic.', },
+  { year: 2018, text: 'Got a Bachelors degree in Clinical Informatics.'},
+  { year: 2018, text: 'Started my CTO role at Toro Clinic.', },
+  { year: 2021, text: 'Got a Masters degree in Biomedical Engineering.', },
 ];
