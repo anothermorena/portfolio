112b4d7d286bee68ecad63ae4f9f156bebb6dfc2
morenamt <onalepeloo@live.com>
2022-08-03 15:09:42 +0200

    added head component and title to the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Hero from '../components/Hero/Hero';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
@@ -9,6 +10,10 @@ import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation'
 const Home = () => {
   return (
     <Layout>
+      <Head>
+        <title>Morena | Personal Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Section grid> 
         <Hero />
         <BgAnimation /> 
