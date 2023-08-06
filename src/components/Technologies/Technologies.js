ef898c620e3afbced8cf6d4f82f6033e9eb20285
Otsogile Onalepelo <otsogile@weblogic.co.bw>
2023-08-06 11:24:49 +0200

    updated my  portfolio

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  FaRobot,
-  FaTools,
-  FaReact,
-  FaDatabase,
-} from 'react-icons/fa';
+import { FaRobot, FaTools, FaReact, FaDatabase } from 'react-icons/fa';
 import {
   List,
   ListContainer,
@@ -20,64 +15,64 @@ import {
 } from '../../styles/GlobalComponents';
 
 const Technologies = () => (
-  <Section id="tech">
+  <Section id='tech'>
     <SectionDivider divider />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
-      I've worked with a range of technologies, from web development
-      through cross-platform mobile development to artificial
-      intelligence.
+      I've worked with a range of technologies, from web development through
+      cross-platform mobile development to artificial intelligence.
     </SectionText>
     <List>
       <ListItem>
         <picture>
-          <FaReact size="2rem" />
+          <FaReact size='2rem' />
         </picture>
         <ListContainer>
           <ListTitle>Front-End</ListTitle>
           <ListParagraph>
-            HTML, CSS, Bootstrap, Tailwind CSS, Materialize CSS,
-            Chakra UI, JavaScript, jQuery, Ajax, React, React
-            Native, Expo, SEO, WPO, PWA
+            HTML, CSS, Bootstrap, Tailwind CSS, Materialize CSS, Chakra UI,
+            JavaScript, jQuery, Ajax, React, React Native, Expo, SEO, WPO, PWA
           </ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
         <picture>
-          <FaDatabase size="2rem" />
+          <FaDatabase size='2rem' />
         </picture>
         <ListContainer>
           <ListTitle>Back-End</ListTitle>
           <ListParagraph>
-            XML, JSON, SQL, MySQL, Redis, PHP, Botman, Restful & SOAP API’s,
-            Python, FastAPI, C++, Twilio, Africa is Talking
+            XML, JSON, SQL, MySQL, Redis, PHP, Botman, Restful & SOAP APIs,
+            Python, FastAPI, C++, Twilio, Node Js, Express Js, Mongo DB Africa
+            is Talking
           </ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
         <picture>
-          <FaRobot size="2rem" />
+          <FaRobot size='2rem' />
         </picture>
         <ListContainer>
           <ListTitle>Artificial Intelligence</ListTitle>
           <ListParagraph>
             ETL, EDA, Data Visualization, NumPy, Pandas, Scikit-learn,
-            Matplotlib, Seaborn, TensorFlow, Keras, Machine & Deep
-            Learning Algorithms, Streamlit, Dialogflow.
+            Matplotlib, Seaborn, TensorFlow, Keras, Machine & Deep Learning
+            Algorithms, Streamlit, Dialogflow, NLP, NLTK, Spacy, Gensim,
+            fastText, Recommender Systems.
           </ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
         <picture>
-          <FaTools size="2rem" />
+          <FaTools size='2rem' />
         </picture>
         <ListContainer>
           <ListTitle>Other Tools & Skils</ListTitle>
           <ListParagraph>
-            Git & GitHub, UML, A/B Testing, Pytest, Jest, Cypress,
-            Web Scraping, Cloud Technologies, Postman, System
-            Design, Design Patterns, People Skills, Presentation
-            Skills, Microsoft Office.
+            Git & GitHub, G it Lab, Docker, UML, A/B Testing, Pytest, Jest,
+            Cypress, Web Scraping, Cloud Technologies, Postman,System Design,
+            Design Patterns, People Skills, Presentation Skills, Linux,
+            Microsoft Office.
           </ListParagraph>
         </ListContainer>
       </ListItem>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,64 +1,80 @@
 export const projects = [
   {
-    title: 'Toro Clinic Website',
-    description:"Toro Clinic is an out patient clinic located in Palapye, Botswana. I developed its  website which plays a vital role in the clinic’s digital marketing and transformation strategy and it makes it easy for clients to schedule appointments online.",
+    title: 'Parcks Website',
+    description:
+      'Currently engaged in the development of a website for a UK-based startup, Parcks, which involves converting UI/UX designs from Figma into functional code. A portion of the website is already live on the companys domain, accessible at https://parcks.com',
     image: '/images/2.png',
-    tags: ['HTML','Bootstrap','JavaScript','PHP','MySQL'],
-    live: 'https://toroclinic.com',
+    tags: ['React, Chakra UI', 'FastAPI', 'MySQL'],
+    live: 'https://parcks.vercel.app/',
     code: '',
     id: 0,
   },
   {
     title: 'NFT Market Place App',
-    description: "A react native(UI), FastAPI (Server) and MySQL(DB) non-fungible token market place where users can create NFTs and bid on other users' NFTs.",
-      image: '/images/1.png',
-      tags: ['Expo', 'MySQL', 'FastAPI'],
+    description:
+      "A react native(UI), FastAPI (Server) and MySQL(DB) non-fungible token market place where users can create NFTs and bid on other users' NFTs.",
+    image: '/images/1.png',
+    tags: ['Expo', 'MySQL', 'FastAPI'],
     live: 'https://github.com/anothermorena/nft-marketplace',
     code: '',
     id: 1,
   },
   {
     title: 'Breast Cancer Classification PWA',
-    description: "An end-to-end PWA and a supervised machine learning (random forest) project which classifies patients’ abnormal masses of cells (tumors) as either benign (non-cancerous) or malignant (cancerous). The model had a 0.99 AUC score, 0.99 Accuracy, 1.0 Precision, 0.97 Recall, and a 0.99 F1-score for class 0 (Cancerous Tumors).",
-      image: '/images/3.png',
-      tags: ['React','Python','FastAPI','Sklearn'],
+    description:
+      'An end-to-end PWA and a supervised machine learning (random forest) project which classifies patients’ abnormal masses of cells (tumors) as either benign (non-cancerous) or malignant (cancerous). The model had a 0.99 AUC score, 0.99 Accuracy, 1.0 Precision, 0.97 Recall, and a 0.99 F1-score for class 0 (Cancerous Tumors).',
+    image: '/images/3.png',
+    tags: ['React', 'Python', 'FastAPI', 'Sklearn'],
     code: 'https://github.com/anothermorena/Breast-Cancer-Classification',
     live: 'https://bcca.vercel.app/',
     id: 2,
   },
   {
     title: 'Medical Insurance Cost Prediction PWA',
-    description: "Another end-to-end PWA and a machine learning (linear regression) project to predict the medical insurance cost of a person. The model yielded an R2 score of 0.79",
+    description:
+      'Another end-to-end PWA and a machine learning (linear regression) project to predict the medical insurance cost of a person. The model yielded an R2 score of 0.79',
     image: '/images/4.png',
-    tags: ['React','Python','FastAPI','Sklearn'],
+    tags: ['React', 'Python', 'FastAPI', 'Sklearn'],
     code: 'https://github.com/anothermorena/Medical-Insurance-Cost-Prediction',
     live: 'https://micpa.vercel.app/',
     id: 3,
   },
   {
     title: 'Pneumonia Prediction PWA',
-    description: "This is an end-to-end PWA and a convolutional neural network (CNN) project that classifies chest x-ray images of patients with lung disease (pneumonia) and those who do not have lung disease (normal). The model had a 0.90 AUC score, 0.91 Accuracy, 0.92 Precision, 0.94 Recall, and a 0.91 F1-score for class 0 (Pneumonia).",
+    description:
+      'This is an end-to-end PWA and a convolutional neural network (CNN) project that classifies chest x-ray images of patients with lung disease (pneumonia) and those who do not have lung disease (normal). The model had a 0.90 AUC score, 0.91 Accuracy, 0.92 Precision, 0.94 Recall, and a 0.91 F1-score for class 0 (Pneumonia).',
     image: '/images/5.png',
-    tags: ['React','Python','FastAPI','Keras', 'Tensorflow'],
+    tags: ['React', 'Python', 'FastAPI', 'Keras', 'Tensorflow'],
     code: 'https://github.com/anothermorena/pneumonia-prediction',
     live: 'https://ppa-virid.vercel.app/',
     id: 4,
   },
   {
     title: 'Ithute Motswana Website',
-    description: "This was a scholarship application portal. I recruited like-minded Batswana and established an NGO, developed its website and social platforms. We sought to help Batswana get international scholarships and managed to help several students win fully funded scholarships.",
+    description:
+      'This was a scholarship application portal. I recruited like-minded Batswana and established an NGO, developed its website and social platforms. We sought to help Batswana get international scholarships and managed to help several students win fully funded scholarships.',
     image: '/images/6.png',
-    tags: ['HTML','Materialize CSS','jQuery','PHP','MySQL'],
+    tags: ['HTML', 'Materialize CSS', 'jQuery', 'PHP', 'MySQL'],
     code: 'https://github.com/anothermorena/Ithute-Motswana',
-    live: 'https://ithute-motswana.000webhostapp.com/',
+    live: 'http://ithutemotswana.com/',
     id: 5,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2021, text: 'Got a Masters degree in Biomedical Engineering specializing in Bioinformatics and Machine Learning.', },
-  { year: 2018, text: 'Started my CTO role at Toro Clinic.', },
-  { year: 2018, text: 'Got a Bachelors degree in Clinical Informatics.'},
-  { year: 2016, text: 'Got an information technology internship at Always Open Clinic.', },
-  { year: 2016, text: 'Started my software engineering journey.', },
+  {
+    year: 2023,
+    text: 'Started my Technical Lead role at Weblogic Botswana.',
+  },
+  {
+    year: 2021,
+    text: 'Got a Masters degree in Biomedical Engineering specializing in Bioinformatics and Machine Learning.',
+  },
+  { year: 2018, text: 'Started my CTO role at Toro Clinic.' },
+  { year: 2018, text: 'Got a Bachelors degree in Clinical Informatics.' },
+  {
+    year: 2016,
+    text: 'Got an information technology internship at Always Open Clinic.',
+  },
+  { year: 2016, text: 'Started my software engineering journey.' },
 ];
